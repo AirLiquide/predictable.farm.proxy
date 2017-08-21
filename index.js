4ebc1866c33010c986a9325e361529daeafee01a
Faouzane BATIGA <dev@enyosolutions.com>
2017-08-21 08:48:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ var customResolver = function (host, url, req) {
 
 customResolver.priority = 1000;
 
-var proxy = require('redbird')({port: 8090, resolvers: [customResolver]});
+var proxy = require('redbird')({port: 80, resolvers: [customResolver]});
 
 proxy.register("http://ecf-berlin.predictable.farm", "http://127.0.0.1:4001");
 proxy.register("http://ecf-berlin.predictable.farm/automation", "http://127.0.0.1:4002/recipes/");
